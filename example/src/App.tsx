a9bec1c2c0acfc49f764542c690dd2d49541eb37
ahmedawaad1804 <ahmedawaad1804@gmail.com>
2025-02-24 10:05:08 +0300

    chore: temp change to package json

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,16 @@
-import { View, StyleSheet, Button } from 'react-native';
-import RNRestart from 'react-native-restart-newarch';
+import { View, StyleSheet, Button, Text } from "react-native";
+import RNRestart from "react-native-restart-newarch";
+const isTurboModulesEnabled = global.__turboModuleProxy !== undefined;
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Button title="Restart" onPress={() => RNRestart.restart()} />
+      <Text>
+        {isTurboModulesEnabled
+          ? "Turbo Modules are enabled"
+          : "Turbo Modules are disabled"}
+      </Text>
     </View>
   );
 }
@@ -12,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

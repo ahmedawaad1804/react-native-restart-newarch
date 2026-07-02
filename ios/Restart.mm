b764298cd7cebd461770e3e25ea2a9532831b79e
Ahmed Awaad <ashahin@aljazirabank.com.sa>
2026-07-02 10:20:18 +0300

    chore(version): bump version to 1.0.86 and optimize restart method

--- ios/Restart.mm
+++ ios/Restart.mm
@@ -2,22 +2,26 @@
 #import <React/RCTReloadCommand.h>
 
 @implementation RestartNewArch
+
 RCT_EXPORT_MODULE()
+
+static BOOL reloadScheduled = NO;
 
 - (void)loadBundle
 {
     RCTTriggerReloadCommandListeners(@"react-native-restart-newarch: RestartNewArch");
 }
 
-RCT_EXPORT_METHOD(restart) {
-    if ([NSThread isMainThread]) {
+RCT_EXPORT_METHOD(restart)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (reloadScheduled) {
+            return;
+        }
+
+        reloadScheduled = YES;
         [self loadBundle];
-    } else {
-        dispatch_sync(dispatch_get_main_queue(), ^{
-            [self loadBundle];
-        });
-    }
-    return;
+    });
 }
 
 #ifdef RCT_NEW_ARCH_ENABLED
